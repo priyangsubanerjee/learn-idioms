db4c6bc45ec38519821ef2d0d2e60b81c1c32638
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-04-09 19:10:27 +0530

    changes in ui

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
 export default function Home() {
   return (
     <div className="h-screen fixed inset-0 flex flex-col">
-      <div className="px-6 mt-6">
+      <div className="px-6 mt-6 hidden">
         <div className="bg-sky-50 p-3 flex items-start">
           <p className="text-sm text-zinc-700">
             Hey, we have got a small collection of idioms till now!{" "}
@@ -30,9 +31,29 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="px-6 mt-10">
-        <h1 className="text-4xl font-playfair font-semibold">Idioms</h1>
-        <div className="flex h-12 bg-zinc-50 border border-zinc-100 rounded-lg mt-6 items-center px-3">
+      <div className="px-6 mt-6">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-zinc-800 tracking-wide font-playfair">
+            Idioms
+          </h1>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 9h16.5m-16.5 6.75h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="flex h-10 bg-zinc-100 rounded-lg mt-6 items-center px-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -57,16 +78,42 @@ export default function Home() {
           />
         </div>
       </div>
+
       <div className="mt-7">
-        <div className="mt-5 overflow-x-auto flex items-center px-6 snap-x snap-mandatory">
-          <div className="flex sticky left-0 z-0 items-center snap-mandatory snap-center snap-always text-blue-500 space-x-3 px-6">
+        <ul className="px-6">
+          <li className="flex py-3 border-b items-center text-blue-500 space-x-3">
+            <i class="bi bi-heart"></i>
+            <span>Favourites</span>
+          </li>
+          <li className="flex py-3 border-b items-center text-blue-500 space-x-3">
+            <i class="bi bi-shuffle"></i>
+            <span>Random cards</span>
+          </li>
+          <li className="flex py-3 border-b items-center text-blue-500 space-x-3">
+            <i class="bi bi-calendar"></i>
+            <span>Idiom of the day</span>
+          </li>
+          <li className="flex py-3 border-b items-center text-blue-500 space-x-3">
+            <i class="bi bi-emoji-laughing"></i>
+            <span>Learn by category</span>
+          </li>
+          <li className="flex py-3 items-center text-blue-500 space-x-3">
+            <i class="bi bi-plus-lg"></i>
+            <span>Contribute</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-4">
+        <div className="mt-5 overflow-x-auto flex items-center px-6 snap-x snap-mandatory lg:snap-none scrollbar-hide">
+          <div className="flex sticky left-0 z-0 items-center snap-mandatory snap-center snap-always bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-fuchsia-500 space-x-2 px-6">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="w-6 h-6"
+              class="w-6 h-6 text-indigo-500"
             >
               <path
                 stroke-linecap="round"
@@ -79,91 +126,45 @@ export default function Home() {
                 d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z"
               />
             </svg>
-            <span>Trending</span>
+            <span className="font-medium">Trending</span>
           </div>
-          <div className="w-screen bg-white z-10 pr-8">
+          <div className="w-screen bg-white z-10 pr-12 lg:pr-6">
             <div className="bg-zinc-50 z-10 w-full rounded-md px-4 py-4 shrink-0 snap-mandatory snap-center snap-always">
-              <h2 className="w-full whitespace-nowrap overflow-hidden text-ellipsis text-zinc-800 font-medium">
+              <p className="text-xs text-indigo-500">200 people love this</p>
+              <h2 className="mt-3 w-full whitespace-nowrap overflow-hidden text-ellipsis text-zinc-800 font-semibold">
                 Idiom phrase
               </h2>
-              <p className="mt-1 w-full whitespace-nowrap overflow-hidden text-ellipsis text-sm text-zinc-600">
+              <p className="mt-1 w-full whitespace-nowrap overflow-hidden text-ellipsis text-sm text-zinc-500">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
                 rem?
               </p>
-              <div className="mt-5 flex justify-end">
-                <button>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="w-5 h-5 text-rose-500"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                    />
-                  </svg>
-                </button>
-              </div>
             </div>
           </div>
-          <div className="w-screen bg-white z-10 pr-8">
+          <div className="w-screen bg-white z-10 pr-12 lg:pr-6">
             <div className="bg-zinc-50 z-10 w-full rounded-md px-4 py-4 shrink-0 snap-mandatory snap-center snap-always">
-              <h2 className="w-full whitespace-nowrap overflow-hidden text-ellipsis text-zinc-800 font-medium">
+              <p className="text-xs text-indigo-500">200 people love this</p>
+              <h2 className="mt-3 w-full whitespace-nowrap overflow-hidden text-ellipsis text-zinc-800 font-semibold">
                 Idiom phrase
               </h2>
-              <p className="mt-1 w-full whitespace-nowrap overflow-hidden text-ellipsis text-sm text-zinc-600">
+              <p className="mt-1 w-full whitespace-nowrap overflow-hidden text-ellipsis text-sm text-zinc-500">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
                 rem?
               </p>
-              <div className="mt-5 flex justify-end">
-                <button>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="w-5 h-5 text-rose-500"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                    />
-                  </svg>
-                </button>
-              </div>
+            </div>
+          </div>
+          <div className="w-screen bg-white z-10 pr-12 lg:pr-6">
+            <div className="bg-zinc-50 z-10 w-full rounded-md px-4 py-4 shrink-0 snap-mandatory snap-center snap-always">
+              <p className="text-xs text-indigo-500">200 people love this</p>
+              <h2 className="mt-3 w-full whitespace-nowrap overflow-hidden text-ellipsis text-zinc-800 font-semibold">
+                Idiom phrase
+              </h2>
+              <p className="mt-1 w-full whitespace-nowrap overflow-hidden text-ellipsis text-sm text-zinc-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+                rem?
+              </p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="mt-7">
-        <ul className="space-y-5">
-          <li className="flex px-6 items-center text-blue-500 space-x-3">
-            <i class="bi bi-heart"></i>
-            <span>Favourites</span>
-          </li>
-          <li className="flex px-6 items-center text-blue-500 space-x-3">
-            <i class="bi bi-shuffle"></i>
-            <span>Random cards</span>
-          </li>
-          <li className="flex px-6 items-center text-blue-500 space-x-3">
-            <i class="bi bi-calendar"></i>
-            <span>Idiom of the day</span>
-          </li>
-          <li className="flex px-6 items-center text-blue-500 space-x-3">
-            <i class="bi bi-emoji-laughing"></i>
-            <span>Learn by category</span>
-          </li>
-          <li className="flex px-6 items-center text-blue-500 space-x-3">
-            <i class="bi bi-plus-lg"></i>
-            <span>Contribute</span>
-          </li>
-        </ul>
       </div>
     </div>
   );
